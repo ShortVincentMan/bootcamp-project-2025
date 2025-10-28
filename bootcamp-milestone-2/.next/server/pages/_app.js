0d6c813775d2d2205f4cedd544a14029ec4d6deb
ShortVincentMan <vinvyle06@gmail.com>
2025-10-27 18:48:23 -0700

    feat: Creating Milestone 2 Now

---
+++ bootcamp-milestone-2/.next/server/pages/_app.js
@@ -0,0 +1,4 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
+R.c("server/chunks/ssr/[root-of-the-server]__eae0f806._.js")
+R.m("[project]/Downloads/Hack4Impact/bootcamp-project-2025/bootcamp-milestone-2/node_modules/next/app.js [ssr] (ecmascript)")
+module.exports=R.m("[project]/Downloads/Hack4Impact/bootcamp-project-2025/bootcamp-milestone-2/node_modules/next/app.js [ssr] (ecmascript)").exports
