a0761a91bac3689e98af1cd477c11bc502d3601b
ShortVincentMan <vinvyle06@gmail.com>
2025-12-04 18:11:39 -0800

    feat: added email feature and imported emailjs library, hid api keys

--- bootcamp-milestone-4/src/app/contact/page.tsx
+++ bootcamp-milestone-4/src/app/contact/page.tsx
@@ -1,24 +1,81 @@
+'use client'
+
 import styles from "./contact.module.css";
+import { useRef, FormEvent } from "react" ;
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef<HTMLFormElement>(null);
+
+  // storing key variables
+  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+  const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+  const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current || !serviceId || !templateId || !publicKey) {
+      alert("Email service not configured properly.");
+      console.error("Missing EmailJS Environment variables");
+      return;
+    }
+
+    emailjs
+    .sendForm(
+      serviceId, 
+      templateId, 
+      form.current, 
+      publicKey
+    )
+    .then(
+    ()=> {
+      alert("Message sent successfully!");
+      form.current?.reset();
+
+    },
+    (error) => {
+      alert("Failed to send message, please try again.");
+      console.log("EmailJS Error:", error);
+    }
+    );
+  }
   return (
-    <main className="main">
+  <main className="main">
       <div className="page-wrap">
         <h1 className="page-title">Contact</h1>
         <div className={styles.wrap}>
           <p>Need to reach out?</p>
           <p>Fill out the form below, or email <a href="mailto:vinvyle06@gmail.com">vinvyle06@gmail.com</a></p>
 
-          <form id="contact-form" className={styles.form}>
+          <form ref={form} id="contact-form" className={styles.form} onSubmit={sendEmail}>
+            <label htmlFor="subject" className={styles.label}>Subject</label>
+            <input className={styles.input}
+            id="subject" 
+            name="subject"
+            required 
+            placeholder="Subject" 
+            />
             <label htmlFor="name" className={styles.label}>Name</label>
-            <input id="name" name="Name" placeholder="Name" className={styles.input} />
-
+            <input className={styles.input}
+            id="name" 
+            name="name" 
+            required
+            placeholder="First and Last Name"  
+            />
             <label htmlFor="email" className={styles.label}>Email</label>
-            <input id="email" type="email" name="Email" placeholder="Email" className={styles.input} />
-
+            <input className={styles.input}
+            id="email" 
+            type="email" 
+            name="email"
+            required
+            placeholder="Email" 
+            />
             <label htmlFor="message" className={styles.label}>Message</label>
-            <textarea id="message" name="Message" rows={6} className={styles.textarea} />
-
+            <textarea className={styles.textarea}
+            id="message" 
+            name="message" 
+            rows={6}  />
             <button type="submit" className={styles.submit}>Submit</button>
           </form>
         </div>
